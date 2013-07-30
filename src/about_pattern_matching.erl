ce600d8bdfd90108d629ef309a37d3bbff7f42b3
cuixin <steven.cuixin@gmail.com>
2013-07-30 18:26:10 +0800

    so easy

--- src/about_pattern_matching.erl
+++ src/about_pattern_matching.erl
@@ -3,32 +3,32 @@
 
 what_looks_like_assignment_might_not_be() ->
   Fruits = apple,
-  Fruits = __.
+  Fruits = 'apple'.
 
 assignment_with_a_tuple() ->
  {X, Y} = {banana, apple},
- (X =:= __) and (Y =:= apple).
+ (X =:= banana) and (Y =:= apple).
 
 branching_on_patterns() ->
   Fruits = {banana, apple},
   case Fruits of
     {banana, Item} ->
-      __ =:= Item
+      apple =:= Item
   end.
 
 sometimes_we_do_not_need_to_know_everything() ->
-  Fruits = {__, banana},
+  Fruits = {apple, banana},
   case Fruits of
-    {apple, _} ->
+    {apple, banana} ->
       get_here
   end.
 
 and_sometimes_we_dont_need_to_match_anything() ->
   case {banana, apple} of
-    {pear, _} ->
+    {pear, Answer} ->
       Answer = pear;
     _ ->
       Answer = anything
   end,
-  __ =:= Answer.
+  anything =:= Answer.
 
